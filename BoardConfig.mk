d3e7cfc8c51b9446bcac1cb99aad7cff42349073
รัชนันท์ ศรีรัตนเมธ <peathot@hotmail.com>
2014-05-02 21:40:13 +0700

    Add dtb image, as needed to boot device.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,17 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x80000000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0xfbd80000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
+#Comment the line below to build kernel from source. (But not for now LOL)
+TARGET_USE_PREBUILT := true
+
+ifeq ($(TARGET_USE_PREBUILT),true)
 TARGET_PREBUILT_KERNEL := device/lge/w7ds/kernel
+BOARD_MKBOOTIMG_ARGS  := --dt device/lge/w7ds/dt.img
+else
+TARGET_KERNEL_SOURCE := kernel/lge/msm8974 #We don't have it yet.
+TARGET_KERNEL_CONFIG := w7ds_open_cis_defconfig
+G2_DTS_TARGET := msm8226-w7ds_open_cis
+BOARD_CUSTOM_BOOTIMG_MK := device/lge/w7ds/mkbootimg.mk
+endif
 
 BOARD_HAS_NO_SELECT_BUTTON := true
