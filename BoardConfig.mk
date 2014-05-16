7c7f0517830a4255a802391c87863fc59ed14aea
รัชนันท์ ศรีรัตนเมธ <peathot@hotmail.com>
2014-05-16 23:52:13 +0700

    Edit BoardConfig.mk to compile kernel.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,8 +13,9 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
 #We have to wait until we decide to build kernel ourself.
-#TARGET_BOARD_PLATFORM := msm8226
+TARGET_BOARD_PLATFORM := msm8226
 ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno305
 
 TARGET_BOOTLOADER_BOARD_NAME := w7ds
 
@@ -33,16 +34,17 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 0xfbd80000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 #Comment the line below to build kernel from source. (But not for now LOL)
-TARGET_USE_PREBUILT := true
+#TARGET_USE_PREBUILT := true
 
 ifeq ($(TARGET_USE_PREBUILT),true)
 TARGET_PREBUILT_KERNEL := device/lge/w7ds/kernel
 BOARD_MKBOOTIMG_ARGS  := --dt device/lge/w7ds/dt.img
 else
-TARGET_KERNEL_SOURCE := kernel/lge/msm8974 #We don't have it yet.
+TARGET_KERNEL_SOURCE := kernel/lge/w7ds
 TARGET_KERNEL_CONFIG := w7ds_open_cis_defconfig
-G2_DTS_TARGET := msm8226-w7ds_open_cis
-BOARD_CUSTOM_BOOTIMG_MK := device/lge/w7ds/mkbootimg.mk
+BOARD_KERNEL_SEPARATED_DT := true
+MSM8226_DTS_TARGET := msm8226-w7ds_open_cis
+BOARD_CUSTOM_BOOTIMG_MK := device/lge/w7ds/releasetools/mkbootimg.mk
 endif
 
 BOARD_HAS_NO_SELECT_BUTTON := true
